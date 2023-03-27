6ec034deee21ed4c8c393d374279b6afcf4dc4d0
Roy Collings <roy.collings@gmail.com>
2023-03-27 20:00:06 +0200

    wip

--- src/pact/interactions/getItems.js
+++ src/pact/interactions/getItems.js
@@ -1,17 +1,22 @@
 const { Matchers } = require("@pact-foundation/pact");
+const { regex, url } = require("@pact-foundation/pact/src/v3/matchers");
+const { term } = Matchers;
 
 module.exports = {
     state: "Getting an item",
     uponReceiving: "a request for an item",
     withRequest: {
         method: "GET",
-        path: "/items/1"
+        path: term({ generate: "/items/1", matcher: "/items/[0-9]" })
     },
 
     willRespondWith: {
         status: 200,
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
         body: {
-            "item_id": Matchers.integer
+            "item_id": Matchers.integer(1)
         }
     }
 };
