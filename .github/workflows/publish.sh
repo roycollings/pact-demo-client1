ca99acff0d3340639d1177a6cbc1648227d6c9b5
Roy Collings <roy.collings@gmail.com>
2023-03-27 11:43:04 +0200

    wip

--- .github/workflows/publish.sh
+++ .github/workflows/publish.sh
@@ -4,7 +4,6 @@ MISSING=()
 [ ! "$PACT_BROKER_BASE_URL" ] && MISSING+=("PACT_BROKER_BASE_URL")
 [ ! "$PACT_BROKER_TOKEN" ] && MISSING+=("PACT_BROKER_TOKEN")
 [ ! "$version" ] && MISSING+=("version")
-[ ! "$pactfiles" ] && MISSING+=("pactfiles")
 
 if [ ${#MISSING[@]} -gt 0 ]; then
   echo "ERROR: The following environment variables are not set:"
@@ -16,7 +15,6 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 
 echo """
 PACT_BROKER_BASE_URL: $PACT_BROKER_BASE_URL
-PACT_BROKER_TOKEN: $PACT_BROKER_TOKEN
 version: $version
 pactfiles: $pactfiles
 branch: $branch
@@ -28,7 +26,6 @@ docker run --rm \
   -e PACT_BROKER_BASE_URL=$PACT_BROKER_BASE_URL \
   -e PACT_BROKER_TOKEN=$PACT_BROKER_TOKEN \
   pactfoundation/pact-cli:latest \
-  publish \
-  $pactfiles \
+  publish ./src/pact/pactfiles \
   --consumer-app-version $version \
   --branch $branch
