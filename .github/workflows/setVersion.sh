40eebb3ffe22e0f5d9396ee0839f36c6e29ce3db
Roy Collings <roy.collings@gmail.com>
2023-03-27 20:12:16 +0200

    wip

--- .github/workflows/setVersion.sh
+++ .github/workflows/setVersion.sh
@@ -2,3 +2,4 @@
 # NOTE: Will only work after actions/checkout
 VERSION=$(node -e 'console.log(require("./package.json").version)')
 echo VERSION=$VERSION >> $GITHUB_ENV
+echo version=$VERSION >> $GITHUB_ENV
